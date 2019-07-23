a9b1e9675924797e18278d65d6bb79faa6602c89
tsyrulalexander <tsyrulalexander@gmail.com>
2019-07-23 23:14:38 +0300

    #0 CreateProject

---
+++ TerritoriesAssignment.WebApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace TerritoriesAssignment.WebApp {
+	public class Program {
+		public static void Main(string[] args) {
+			BuildWebHost(args).Run();
+		}
+
+		public static IWebHost BuildWebHost(string[] args) {
+			return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+		}
+	}
+}
---
+++ TerritoriesAssignment.WebApp/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TerritoriesAssignment.WebApp {
+	public class Startup {
+		public void ConfigureServices(IServiceCollection services) {
+
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+			if (env.IsDevelopment()) {
+				app.UseDeveloperExceptionPage();
+				app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions {
+					HotModuleReplacement = true
+				});
+			}
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+		}
+	}
+}
