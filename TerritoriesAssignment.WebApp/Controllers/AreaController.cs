7a67037e9e6d0a857dd09f37067cb570e233a3ad
Alexander Tsyrul <tsyrulalexander@gmail.com>
2019-07-30 20:38:04 +0300

    #0 + Area and Region controllers

---
+++ TerritoriesAssignment.WebApp/Controllers/AreaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TerritoriesAssignment.Core.Db;
+using TerritoriesAssignment.Core.Entities;
+
+namespace TerritoriesAssignment.WebApp.Controllers {
+	[Route("api/area")]
+	public class AreaController : Controller, IStorageController<Area> {
+		public IDataStorage Storage { get; }
+
+		public AreaController(IDataStorage storage) {
+			Storage = storage;
+		}
+		[HttpGet("{countryId}")]
+		public IEnumerable<Area> GetItems(Guid countryId) {
+			return Storage.GetAreas(countryId);
+		}
+		public Area Get(Guid id) {
+			return Storage.GetArea(id);
+		}
+		public void Post(Area item) {
+			Storage.AddArea(item);
+		}
+		public void Put(Area item) {
+			Storage.UpdateArea(item);
+		}
+		public void Delete(Guid id) {
+			Storage.DeleteArea(id);
+		}
+	}
+}
---
+++ TerritoriesAssignment.WebApp/Controllers/RegionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TerritoriesAssignment.Core.Db;
+using TerritoriesAssignment.Core.Entities;
+
+namespace TerritoriesAssignment.WebApp.Controllers {
+	public class RegionController : Controller, IStorageController<Region> {
+		public IDataStorage Storage { get; }
+
+		public RegionController(IDataStorage storage) {
+			Storage = storage;
+		}
+		[HttpGet("{areaId}")]
+		public IEnumerable<Region> GetItems(Guid areaId) {
+			return Storage.GetRegions(areaId);
+		}
+		public Region Get(Guid id) {
+			return Storage.GetRegion(id);
+		}
+		public void Post(Region item) {
+			Storage.AddRegion(item);
+		}
+		public void Put(Region item) {
+			Storage.UpdateRegion(item);
+		}
+		public void Delete(Guid id) {
+			Storage.DeleteRegion(id);
+		}
+	}
+}
